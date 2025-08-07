676a42c88cd62d432c68a27e1bf908bc512d8b25
BRUNO_PESSOA <202503440964>
2025-08-07 18:31:56 -0300

    .

---
+++ src/pages/Sucesso.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Botao from '../components/Botao';
+import Footer from '../components/Footer';
+
+function Sucesso() {
+  return (
+    <>
+      <main className="container corpo">
+        <section className="info-section success-message">
+          <h2>Inscrição Enviada com Sucesso!</h2>
+          <p>Obrigado por se inscrever no CanSat Ibmec. Em breve entraremos em contato com mais informações.</p>
+          <Botao to="/">Voltar para a Página Inicial</Botao>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Sucesso;
